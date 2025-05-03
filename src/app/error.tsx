54bbc4d3399ed638028ac9265029d2d11b038901
samssjubair <samssjubair@gmail.com>
2025-05-03 17:07:16 +0600

    folder structure

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Error = () => {
+    return (
+    <div className="flex justify-center items-center h-screen">
+        <h1 className="text-red-500 text-4xl font-bold">Something Went Wrong!</h1>
+    </div>
+    );
+};
+
+export default Error;
---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+    return (
+    <div className="flex justify-center items-center h-screen">
+        <h1 className="text-4xl font-bold">Loading...</h1>
+    </div>
+    );
+};
+
+export default Loading;
